1995709bee71d1329dbb89ecaa09d443812b9a17
kl1947 <34678260+kl1947@users.noreply.github.com>
2021-09-29 11:53:19 +0200

    Update aiscredentials.py

--- aiscredentials.py
+++ aiscredentials.py
@@ -1,10 +1,10 @@
 # aiscredentials.py: Passwords etc. separated from Code
-# dj8kl, 2021-09-16
+# dj8kl, 2021-09-23
 
 DEBUG = 0
 PRINT_TO_CONSOLE = False
 SEND_TO_APRS = True
-SEND_TO_LOCAL = False
+
 COLLECT_TIME = 60 # Intervall vor collction of AIS messages per MMSI
                   # Seconds, Standard 60 or 120, Mininum 10
 NMEA_FILTER = ['!AIVDM', '!AIVDO'] # only these NMEA records
@@ -12,20 +12,19 @@ NMEA_FILTER = ['!AIVDM', '!AIVDO'] # only these NMEA records
 INPUT_PROTO = 'SER' #'/root/tmp/nmea2.txt'  #'SER', 'TCP', 'UDP', 'FIL'
 INPUT_IP = '/dev/ttyUais' #'Z:/home/pi/aisRecords.txt' #'rpi3weewx' # or Serial Port or Filename
                        # '/dev/ttyUais', '/dev/serial0', '~/aistestdata.txt', 192.168.58.24, 'rpi3weewx' 
-INPUT_PORT = 10110 # 
+INPUT_PORT = 5006 # 
 
 # aprs.fi
-APRS_IP = 'https://aprs.fi/jsonais/post/McHsZQ3PuJ'
-APRS_CLIENT = 'dj8kl'
+APRS_IP = 'https://aprs.fi/jsonais/post/<ID>'
+APRS_CLIENT = 'nnnnn'
 
-# marinetraffic.com: 5.9.207.224 
-# fleetmon.com: 148.251.96.197 
+# marinetraffic.com: 5.9.207.224 Station ID/Port
+#                    https://www.marinetraffic.com/en/ais/details/stations/<id>/
+# fleetmon.com: 148.251.96.197 Station ID/Port
+#               https://www.fleetmon.com/my/ais-stations/<id>
+# EVERY: override collection of packets = send every
 OUT_LIST = {
-  'Marinetraffic': {'ACTIVE': True, 'PROTO': 'TCP', 'IP': '5.9.207.224', 'PORT': <port>},
-  'Fleetmon': {'ACTIVE': True, 'PROTO': 'TCP', 'IP': '148.251.96.197', 'PORT': <port>},
+  'Marinetraffic': {'ACTIVE': True, 'PROTO': 'UDP', 'IP': '5.9.207.224', 'PORT': <PORT>, 'EVERY': False},
+  'Fleetmon': {'ACTIVE': True, 'PROTO': 'UDP', 'IP': '148.251.96.197', 'PORT': <PORT>, 'EVERY': False},
+  'Local': {'ACTIVE': True, 'PROTO': 'UDP', 'IP': '192.168.58.24', 'PORT': 5005, 'EVERY': True},
   }
-  
-# local output for test purposes TODO: allow more than one listener
-LOCAL_PROTO = 'TCP'
-LOCAL_IP = '192.168.58.24'
-LOCAL_PORT = 10110
